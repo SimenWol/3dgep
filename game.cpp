5f1966d22aaa33bc80e47a6f487bc677a2c6049d
MrGam3z <68946439+MrGam3z@users.noreply.github.com>
2022-12-06 20:18:42 +0100

    Changed code as suggested.
    - Standard variables for colours
    - Dynamic array to store lines
    - Don't use Game::Init() for line storage

--- game.cpp
+++ game.cpp
@@ -5,55 +5,49 @@
 
 namespace Tmpl8
 {
+	static constexpr Pixel RED = 0xff0000;
+	static constexpr Pixel GREEN = 0x00ff00;
+	static constexpr Pixel BLUE = 0x0000ff;
+	static constexpr Pixel YELLOW = 0xffff00;
 
-	struct linepoints {
-		int x1;
-		int y1;
-		int x2;
-		int y2;
-		int color;
-	};
-	linepoints codewordArray[28];
-
-	void Game::Init()
+	struct Line
 	{
-		// letter C (red)
-		codewordArray[0] = { 240, 60, 160, 60, 0xff0000 };
-		codewordArray[1] = { 160, 60, 80, 120, 0xff0000 };
-		codewordArray[2] = { 80, 120, 80, 180, 0xff0000 };
-		codewordArray[3] = { 80, 180, 160, 240, 0xff0000 };
-		codewordArray[4] = { 160, 240, 240, 240, 0xff0000 };
+		vec2 p0, p1; // Begin & end line points.
+		Pixel c;	 // Line colour
+	};
 
-		// letter O (green)
-		codewordArray[5] = { 400, 60, 320, 120, 0x00ff00 };
-		codewordArray[6] = { 320, 120, 320, 180, 0x00ff00 };
-		codewordArray[7] = { 320, 180, 400, 240, 0x00ff00 };
-		codewordArray[8] = { 400, 240, 480, 240, 0x00ff00 };
-		codewordArray[9] = { 480, 240, 560, 180, 0x00ff00 };
-		codewordArray[10] = { 560, 180, 560, 120, 0x00ff00 };
-		codewordArray[11] = { 560, 120, 480, 60, 0x00ff00 };
-		codewordArray[12] = { 480, 60, 400, 60, 0x00ff00 };
-		codewordArray[13] = { 400, 120, 400, 180, 0x00ff00 };
-		codewordArray[14] = { 400, 180, 480, 180, 0x00ff00 };
-		codewordArray[15] = { 480, 180, 480, 120, 0x00ff00 };
-		codewordArray[16] = { 480, 120, 400, 120, 0x00ff00 };
+	const Line text[] = {
+		{ {240, 60},  {160, 60},  RED },    // Letter C
+		{ {160, 60},  {80, 120},  RED },
+		{ {80, 120},  {80, 180},  RED },
+		{ {80, 180},  {160, 240}, RED },
+		{ {160, 240}, {240, 240}, RED },
+		{ {400, 60},  {320, 120}, GREEN },  // Letter O
+		{ {320, 120}, {320, 180}, GREEN },
+		{ {320, 180}, {400, 240}, GREEN },
+		{ {400, 240}, {480, 240}, GREEN },
+		{ {480, 240}, {560, 180}, GREEN },
+		{ {560, 180}, {560, 120}, GREEN },
+		{ {560, 120}, {480, 60},  GREEN },
+		{ {480, 60},  {400, 60},  GREEN },
+		{ {400, 120}, {400, 180}, GREEN },
+		{ {400, 180}, {480, 180}, GREEN },
+		{ {480, 180}, {480, 120}, GREEN },
+		{ {480, 120}, {400, 120}, GREEN },
+		{ {80, 300},  {80, 480},  BLUE },   // Letter D
+		{ {80, 480},  {160, 480}, BLUE },
+		{ {160, 480}, {240, 420}, BLUE },
+		{ {240, 420}, {240, 360}, BLUE },
+		{ {240, 360}, {160, 300}, BLUE },
+		{ {160, 300}, {80, 300},  BLUE },
+		{ {160, 360}, {160, 420}, BLUE },
+		{ {480, 300}, {320, 300}, YELLOW }, // Letter E
+		{ {320, 300}, {320, 480}, YELLOW },
+		{ {320, 480}, {480, 480}, YELLOW },
+		{ {320, 420}, {400, 420}, YELLOW }
+	};
 
-		// letter D (blue)
-		codewordArray[17] = { 80, 300, 80, 480, 0x0000ff };
-		codewordArray[18] = { 80, 480, 160, 480, 0x0000ff };
-		codewordArray[19] = { 160, 480, 240, 420, 0x0000ff };
-		codewordArray[20] = { 240, 420, 240, 360, 0x0000ff };
-		codewordArray[21] = { 240, 360, 160, 300, 0x0000ff };
-		codewordArray[22] = { 160, 300, 80, 300, 0x0000ff };
-		codewordArray[23] = { 160, 360, 160, 420, 0x0000ff };
-
-		// letter E (yellow)
-		codewordArray[24] = { 480, 300, 320, 300, 0xffff00 };
-		codewordArray[25] = { 320, 300, 320, 480, 0xffff00 };
-		codewordArray[26] = { 320, 480, 480, 480, 0xffff00 };
-		codewordArray[27] = { 320, 420, 400, 420, 0xffff00 };
-	}
-
+	void Game::Init() {}
 	void Game::Shutdown() {}
 
 	void Game::Tick(float deltaTime)
@@ -61,8 +55,8 @@ namespace Tmpl8
 		// clear the graphics window
 		screen->Clear(0);
 
-		for (linepoints l : codewordArray) {
-			screen->Line(l.x1, l.y1, l.x2, l.y2, l.color);
+		for (auto& line : text) {
+			screen->Line(line.p0.x, line.p0.y, line.p1.x, line.p1.y, line.c);
 		}
 	}
 };
