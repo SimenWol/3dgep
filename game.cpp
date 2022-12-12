ac9a56afa03f45b419a066ce56e862ab1d619251
MrGam3z <68946439+MrGam3z@users.noreply.github.com>
2022-12-12 21:20:17 +0100

    Completed assignment. - Added a nested for loop to fill the whole screen with tanks.

--- game.cpp
+++ game.cpp
@@ -4,37 +4,21 @@
 
 namespace Tmpl8
 {
-	// -----------------------------------------------------------
-	// Initialize the application
-	// -----------------------------------------------------------
-	void Game::Init()
-	{
-	}
-	
-	// -----------------------------------------------------------
-	// Close down application
-	// -----------------------------------------------------------
-	void Game::Shutdown()
-	{
-	}
+	void Game::Init() {}
+	void Game::Shutdown() {}
 
-	static Sprite rotatingGun(new Surface("assets/aagun.tga"), 36);
-	static int frame = 0;
+	Sprite theSprite(new Surface("assets/ctankbase.tga"), 16);
 
-	// -----------------------------------------------------------
-	// Main application tick function
-	// -----------------------------------------------------------
-	void Game::Tick(float deltaTime)
-	{
-		// clear the graphics window
-		screen->Clear(0);
-		// print something in the graphics window
-		screen->Print("hello world", 2, 2, 0xffffff);
-		// print something to the text window
-		printf("this goes to the console window.\n");
-		// draw a sprite
-		rotatingGun.SetFrame(frame);
-		rotatingGun.Draw(screen, 100, 100);
-		if (++frame == 36) frame = 0;
-	}
+    void Game::Tick(float deltaTime)
+    {
+        screen->Clear(0);
+        for (int y = 0; y < 10; y++)
+        {
+            for (int x = 0; x < 16; x++)
+            {
+                theSprite.SetFrame(x);
+                theSprite.Draw(screen, x * 50, y * 50);
+            }
+        }
+    }
 };
