0aaae31025917700a384cbe4fc6a17d197cfc03a
MrGam3z <68946439+MrGam3z@users.noreply.github.com>
2023-01-31 20:10:28 +0100

    [PRE ASSIGNMENT]
    Finished the tutorial, no  assignment done yet.

--- game.cpp
+++ game.cpp
@@ -18,23 +18,23 @@ namespace Tmpl8
 	{
 	}
 
-	static Sprite rotatingGun(new Surface("assets/aagun.tga"), 36);
-	static int frame = 0;
+	Sprite theSprite(new Surface("assets/ball.png"), 1);
 
-	// -----------------------------------------------------------
-	// Main application tick function
-	// -----------------------------------------------------------
+	float spriteY = 0, speed = 1;
 	void Game::Tick(float deltaTime)
 	{
-		// clear the graphics window
+		printf("%f\n", deltaTime);
+
 		screen->Clear(0);
-		// print something in the graphics window
-		screen->Print("hello world", 2, 2, 0xffffff);
-		// print something to the text window
-		printf("this goes to the console window.\n");
-		// draw a sprite
-		rotatingGun.SetFrame(frame);
-		rotatingGun.Draw(screen, 100, 100);
-		if (++frame == 36) frame = 0;
+		spriteY += speed;
+		speed += 1.0f;
+
+		if (spriteY > (512 - 50))
+		{
+			spriteY = 512 - 50;
+			speed = -speed * 0.8f;
+		}
+
+		theSprite.Draw(screen, 20, (int)spriteY);
 	}
 };
